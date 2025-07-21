f02c337e543947ad1e9abc7d341db9fee6055a04
learnersubha <learnersubha0@gmail.com>
2025-07-21 17:44:42 +0530

    Update terraform.tf

--- EKS-cluster/terraform.tf
+++ EKS-cluster/terraform.tf
@@ -1,8 +1,9 @@
 terraform {
+  required_version = ">= 1.0"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "5.92.0"
+      source  = "hashicorp/aws"
+      version = "~> 5.70"
     }
   }
 }
