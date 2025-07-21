6255b926052b9e3a69a9806c26559c459aaf194d
learnersubha <learnersubha0@gmail.com>
2025-07-21 17:45:59 +0530

    Update vpc.tf

--- EKS-cluster/vpc.tf
+++ EKS-cluster/vpc.tf
@@ -1,19 +1,18 @@
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "${local.name}-vpc" 
-  cidr = local.vpc_cidr
-
-  azs = local.azs
-  private_subnets = local.private_subnets
-  public_subnets  = local.public_subnets
-  intra_subnets = local.intra_subnets
-
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.13"  # This version works with AWS provider 5.x
+  
+  # rest of your VPC configuration stays the same
+  name = "${var.name}-vpc"
+  cidr = var.cidr
+  azs = var.azs
+  private_subnets = var.private_subnets
+  public_subnets = var.public_subnets
+  intra_subnets = var.intra_subnets
   enable_nat_gateway = true
   enable_vpn_gateway = true
-
   tags = {
     Terraform = "true"
-    Environment = local.env
+    Environment = var.env
   }
 }
