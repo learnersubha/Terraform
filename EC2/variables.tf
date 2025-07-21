640393ff1d85d2c7af2ab8f20ce74a1c6888f13b
learnersubha <learnersubha0@gmail.com>
2025-07-21 17:49:36 +0530

    Update variables.tf

--- EC2/variables.tf
+++ EC2/variables.tf
@@ -1,12 +1,21 @@
-variable "ec2_instance_type" {
-      default = "t2.micro"
-      type = string
+variable "ec2_ami_id"{
+    default = "ami-042b4708b1d05f512"
+    type = string
 }
-variable "ec2_root_volume_size" {
-    default = 8
+
+variable "ec2_volume" {
+    default = 10
     type = number
 }
-variable "ec2_ami_id" {
-    default = "ami-03fd334507439f4d1"
+
+variable "ec2_instance_name" {
+    default = "terra_server"
     type = string
+  
+}
+
+variable "ec2_instance_type" {
+    default = "t3.micro"
+    type = string
+  
 }
