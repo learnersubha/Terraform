a0e1a8ff6d3bc9029080e5e98a9f6e5cacb470db
learnersubha <learnersubha0@gmail.com>
2025-07-21 17:49:00 +0530

    Update outputs.tf

--- EC2/outputs.tf
+++ EC2/outputs.tf
@@ -1,9 +1,14 @@
-output "ec2_public_ip" {
-    value = aws_instance.microft.public_ip 
+output "ec2_public_ip"{
+    value = aws_instance.demo_instanmce.public_ip
+    
 }
-output "ec2_public_dns" {
-    value = aws_instance.microft.public_dns
+
+output "ec2_public_dns"{
+    value = aws_instance.demo_instanmce.public_dns
+    
 }
-output "ec2_private_ip" {
-    value = aws_instance.microft.private_ip
+
+output "ec2_private_ip"{
+    value = aws_instance.demo_instanmce.private_ip
+    
 }
