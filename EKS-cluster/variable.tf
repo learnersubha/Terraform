d3dc988e9281dead1f335c6167d42fc5f892078e
learnersubha <learnersubha0@gmail.com>
2025-07-21 17:45:03 +0530

    Update variable.tf

--- EKS-cluster/variable.tf
+++ EKS-cluster/variable.tf
@@ -1,10 +1,28 @@
-locals {
-    region = "eu-west-1"
-    name = "microft-eks-cluster"
-    vpc_cidr = "10.0.0.0/16"
-    azs             = ["eu-west-1a", "eu-west-1b"]
-    private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-    public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
-    intra_subnets  = ["10.0.4.0/24", "10.0.5.0/24"]
-    env = "dev"
+variable "name" {
+default = "demo-eks"
+type = string
+}
+variable "cidr" {
+default = "10.0.0.0/16"
+type = string
+}
+variable "azs" {
+default = ["eu-north-1a", "eu-north-1b"]
+type = list(string)
+}
+variable "private_subnets" {
+default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+type = list(string)
+}
+variable "public_subnets" {
+default = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+type = list(string)
+}
+variable "env" {
+default = "dev"
+type = string
+}
+variable "intra_subnets" {
+default = ["10.0.45.0/24", "10.0.46.0/24", "10.0.47.0/24"]
+type = list(string)
 }
