0cacc31c0dde3fcb1a7baee0355da0faa7b04d18
learnersubha <learnersubha0@gmail.com>
2025-07-21 17:43:58 +0530

    Update eks.tf

--- EKS-cluster/eks.tf
+++ EKS-cluster/eks.tf
@@ -1,59 +1,48 @@
 module "eks" {
- # import module template
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.31"
- # cluster information (control-plane)
-  cluster_name    = local.name
-  cluster_version = "1.31"
+## Import module template
+source = "terraform-aws-modules/eks/aws"
+version = "~> 20.24" 
+## Cluster-info (Control-plane)
+cluster_name = var.name
+cluster_endpoint_public_access = true
+vpc_id = module.vpc.vpc_id 
+subnet_ids = module.vpc.private_subnets
 
-  cluster_endpoint_public_access = true
+cluster_addons = {
+vpc-cni = {
+most-recent = true
+}
+kube-proxy = {
+most-recent = true
+}
+core-dns = {
+most-recent = true
+}
 
-  vpc_id  =  module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
-   
-  cluster_addons = {
+}
 
-     vpc_cni = {
-        most-recent = true
-     }
-     kube-proxy = {
-        most-recent = true
-     }
-     core-dns = {
-        most-recent = true
-     }
-  
-  }
+## Control plane network
+control_plane_subnet_ids = module.vpc.intra_subnets
 
- # control plane network
-  control_plane_subnet_ids = module.vpc.intra_subnets
-  
- # manage nodes in the cluster
-  eks_managed_node_group_defaults = {
-  instance_types = ["t2.micro"]
-  attach_cluster_primary_security_group = true
-  
-  }
+## Manage nodes in your cluster
+eks_managed_node_group_defaults = {
 
+instance_types = ["t3.micro"]
+attach_cluster_primary_security_group = true
+}
+eks_managed_node_groups = {
+eks-demo-ng = {
+instance_types = ["t3.micro"]
 
-  eks_managed_node_groups = {
-    microft-cluster-ng = {
-   
-      instance_types = ["t2.micro"]
-
-      min_size     = 1
-      max_size     = 3
-      desired_size = 1
-
-      capacity_type = "SPOT"
-    }
-  }
-
-
-
-  tags = {
-  Environment = local.env
-  Terraform   = "true"
-  }
+min_size = 1
+max_size = 3
+desired_size = 1
+capacity_type = "SPOT"
+}
+}
+tags = {
+Environment = var.env
+Terraform = "true"
+}
 
 }
